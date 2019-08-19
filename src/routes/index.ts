7a9c67c303138aae7bfced2cab9eeff6716d6f8c
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-08-19 22:32:43 +0200

    yiss. get events.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,20 @@
-import { Router } from 'express';
-import UserRouter from './Users';
+import {Request, Response, Router} from 'express';
+import {A3sDirectory} from 'arma3sync-lib';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/users', UserRouter);
+// router.use('/users', UserRouter);
+
+router.get('/events', async (req: Request, res: Response) => {
+    const events = await new A3sDirectory('/home/fusselwurm/arma3/mods/.a3s').getEvents();
+    res.send(JSON.stringify(events));
+});
+
+router.post('/events', async (req: Request, res: Response) => {
+    res.send();
+});
 
 // Export the base-router
 export default router;
