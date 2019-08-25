d3022ed97648bef1b94b15d7a4f4d81608d19ad0
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-08-25 14:47:08 +0200

    feature-complete event editing
    	* delete events
    	* create new events
    	* edit names & descriptions

--- src/entities/Addons.ts
+++ src/entities/Addons.ts
@@ -1,4 +1,4 @@
-import {A3sSyncTreeDirectory, A3sSyncTreeLeaf, A3sSyncTreeNode} from 'arma3sync-lib/dist/model/a3sSync';
+import {A3sSyncTreeDirectory, A3sSyncTreeNode} from 'arma3sync-lib/dist/model/a3sSync';
 
 export class Addons {
     constructor(private a3sSyncTree: A3sSyncTreeDirectory) {
--- src/public/scripts/events.js
+++ src/public/scripts/events.js
@@ -1,58 +1,77 @@
-let eventsModel;
+let eventsModel = [];
 let missingMods = [];
 let availableAddons = [];
 
-displayEvents();
+reload();
 
-function displayEvents() {
+const identity = (x) => x;
+
+function reload() {
     Promise
         .all([httpGet('/api/addons').then(res => res.json()), httpGet('/api/events').then(res => res.json())])
         .then((result) => {
             availableAddons = result[0].map(a => a.name);
             eventsModel = result[1];
 
-            let eventContainer = document.getElementById('event-list');
-            let thList = eventsModel.map(event => `<th title="${event.description}">${event.name}</th>`);
-            let rows = availableAddons.map(mod => {
-                let modCell = `<th scope="row">${mod}</th>`;
-                let cells = eventsModel.map(event => {
-                    let isActive = event.addonNames.indexOf(mod) !== -1;
-                    let id = `checkbox_${event.name}_${mod}`;
-                    return `
-                    <td>
-                        <input type="checkbox"
-                            data-event="${event.name}"
-                            data-addon="${mod}"
-                            data-initial="${isActive}" ${isActive ? "checked" : ""}
-                            id="${id}"
-                        />
-                        <label for="${id}"></label>
-                    </td>`
-                });
-                return `<tr>${modCell} ${cells.join('')}</tr>`
-            });
-
-            eventContainer.innerHTML = `
-                <table>
-                    <thead>
-                        <th></th>${thList.join('')}
-                    </thead>
-                    <tbody>
-                        ${rows.join('\n')}
-                    </tbody>
-                    <tfoot><th></th>${thList.join('')}</tfoot>
-                </table>
-                `;
-            eventContainer.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
-                checkbox.addEventListener('change', checkboxListener);
-            });
-
-            handleMissingMods();
+            displayEvents();
         });
 }
 
+function displayEvents() {
+    let eventContainer = document.getElementById('event-list');
+    let thList = eventsModel.filter(identity).map((event, idx) => `
+        <th title="${event.description}" data-event-index="${idx}">
+            <span>${event.name}</span>
+            <div class="event-controls">
+            <button title="edit" name="edit_event" data-event-index="${idx}">e</button>
+            <button title="delete" name="delete_event" data-event-index="${idx}">x</button>
+            </div>
+        </th>`);
+    let rows = availableAddons.map(mod => {
+        let modCell = `<th scope="row">${mod}</th>`;
+        let cells = eventsModel.filter(identity).map((event, idx) => {
+            let isActive = event.addonNames.indexOf(mod) !== -1;
+            let id = `checkbox_${idx}_${mod}`;
+            return `
+                <td>
+                    <input type="checkbox"
+                        data-event-index="${idx}"
+                        data-addon="${mod}"
+                        data-initial="${isActive}" ${isActive ? "checked" : ""}
+                        id="${id}"
+                    />
+                    <label for="${id}"></label>
+                </td>`
+        });
+        return `<tr>${modCell} ${cells.join('')}</tr>`
+    });
+
+    eventContainer.innerHTML = `
+            <table>
+                <thead>
+                    <th></th>${thList.join('')}
+                </thead>
+                <tbody>
+                    ${rows.join('\n')}
+                </tbody>
+                <tfoot><th></th>${thList.join('')}</tfoot>
+            </table>
+            `;
+    eventContainer.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+        checkbox.addEventListener('change', checkboxListener);
+    });
+    eventContainer.querySelectorAll('button[name=delete_event]').forEach(button => {
+        button.addEventListener('click', deleteButtonListener);
+    });
+    eventContainer.querySelectorAll('button[name=edit_event]').forEach(button => {
+        button.addEventListener('click', editButtonListener);
+    });
+
+    handleMissingMods();
+}
+
 function handleMissingMods() {
-    missingMods = getMissingAddonsThatAreInEvents(eventsModel);
+    missingMods = getMissingAddonsThatAreInEvents(eventsModel.filter(identity));
     if (missingMods.length > 0) {
         alert(`DANGER! these mods seem to not be available, but they are part of existing events: ${missingMods.join(', ')}`);
         document.getElementById('purge-missing-mods').style.display = 'block';
@@ -73,7 +92,10 @@ function getMissingAddonsThatAreInEvents(events) {
 
 function checkboxListener() {
     const checked = this.checked;
-    const eventModel = eventsModel.find(event => event.name === this.dataset.event);
+    const eventModel = eventsModel[this.dataset.eventIndex];
+    if (!eventModel) {
+        throw new Error('wait wtf. couldnt find event ' + this.dataset.eventIndex);
+    }
     const addon = this.dataset.addon;
     const eventAddonIndex = eventModel.addonNames.indexOf(addon);
     if ((eventAddonIndex !== -1) && !checked) {
@@ -81,6 +103,37 @@ function checkboxListener() {
     }
     if ((eventAddonIndex === -1) && checked) {
         switchAddonInEvent(eventModel, addon, true);
+    }
+}
+
+function deleteButtonListener() {
+    const index = this.dataset.eventIndex;
+    eventsModel[index] = undefined;
+    displayEvents();
+}
+
+function editButtonListener() {
+    let index = this.dataset.eventIndex;
+    let eventModel = eventsModel[index];
+
+    let nameAndDesc = promptForNameAndDescription();
+    eventModel.name = nameAndDesc.name;
+    eventModel.description = nameAndDesc.description;
+
+    displayEvents();
+}
+
+function promptForNameAndDescription() {
+    let name = prompt('please enter event name:');
+    if (!name) {
+        alert('nah dude I insist on a name');
+        throw new Error('user did not provide a name');
+    }
+    let description = prompt('please enter event description:') || '';
+
+    return {
+        name: name,
+        description: description
     }
 }
 
@@ -93,9 +146,10 @@ function switchAddonInEvent(eventModel, addonName, newState) {
     }
 }
 
+document.querySelector('#new-event').addEventListener('click', newEvent);
 document.querySelector('#save-event-list').addEventListener('click', submitEdit);
 document.querySelector('#reload-event-list').addEventListener('click', () => {
-    displayEvents();
+    reload();
     document.querySelector('#purge-missing-mods').style.background = 'none';
 });
 document.querySelector('#purge-missing-mods').addEventListener('click', () => {
@@ -103,14 +157,23 @@ document.querySelector('#purge-missing-mods').addEventListener('click', () => {
     document.querySelector('#purge-missing-mods').style.background = 'red';
 });
 
+function newEvent() {
+    let nameAndDescription = promptForNameAndDescription();
+    eventsModel.push({
+        name: nameAndDescription.name,
+        description: nameAndDescription.description,
+        addonNames: [],
+    });
+    displayEvents();
+}
 
 function submitEdit() {
     document.getElementById('save-event-list').enabled = false;
     document.getElementById('reload-event-list').enabled = false;
-    httpPut('/api/events', eventsModel).then(() => {
+    httpPut('/api/events', eventsModel.filter(identity)).then(() => {
         document.getElementById('save-event-list').enabled = true;
         document.getElementById('reload-event-list').enabled = true;
-        displayEvents();
+        reload();
     }, (err) => {
         console.log(err);
         alert('halp, something bad happened');
@@ -118,7 +181,7 @@ function submitEdit() {
 }
 
 function purgeMissingMods() {
-    eventsModel.forEach(eventModel => {
+    eventsModel.filter(identity).forEach(eventModel => {
         missingMods.forEach(missingMod => {
             switchAddonInEvent(eventModel, missingMod, false);
         });
