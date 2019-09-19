7dbbe501c6c2022e240ac060b492f3b712d01bb2
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-09-19 22:30:57 +0200

    set umask so that group can write

--- src/start.ts
+++ src/start.ts
@@ -2,6 +2,8 @@ import app from './Server';
 import {logger} from './shared';
 import {get} from 'config';
 
+process.umask(0o002); // let user group read-write whatever we change
+
 // Start the server
 const port = get('port');
 app.listen(port, () => {
