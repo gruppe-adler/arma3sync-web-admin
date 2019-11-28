58318f95a76cf10fa00e4417e017216e1b5c8320
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-11-28 06:13:29 +0100

    ActionResponses spec

--- src/entities/ActionResponses.spec.ts
+++ src/entities/ActionResponses.spec.ts
@@ -1,11 +1,15 @@
-import {ActionResponses} from 'src/entities/ActionResponses';
+import {ActionResponses, Status} from 'src/entities/ActionResponses';
 
 describe(ActionResponses.name, () => {
+    let responses: ActionResponses;
     describe('get', () => {
-        const responses = new ActionResponses(
-            {5: { status: 'PENDING', message: 'foo'}},
-            3,
-        );
+        beforeEach(() => {
+            responses = new ActionResponses(
+                {5: { status: Status.PENDING, message: 'foo'}},
+                3,
+            );
+        });
+
         it('returns the unknown response if nothing is set', () => {
             const unknown1 = responses.get(1);
             expect(unknown1.status).toBe('UNKNOWN');
@@ -13,18 +17,44 @@ describe(ActionResponses.name, () => {
         it('returns responses that it was initialized with', () => {
             expect(responses.get(5).message).toBe('foo');
         });
+        it('forgets old responses if storage is exceeded', () => {
+            expect(responses.get(5).status).not.toBe('UNKNOWN');
+            responses.add();
+            responses.add(); // now we've got 5,6,7
+            responses.add(); // now we'd have 5,6,7,8, and…
+            expect(responses.get(5).status).toBe('UNKNOWN', 'too-old response has not been forgotten');
+            expect(responses.get(6).status).toBe('NEW', 'recent response has been forgotten :(');
+        });
+    });
+    describe('add', () => {
+        beforeEach(() => {
+            responses = new ActionResponses(
+                {5: { status: Status.PENDING, message: 'foo'}},
+                3,
+            );
+        });
+
         it('adds responses with the next higher index', () => {
             const nextIndex = responses.add();
             expect(nextIndex).toBe(6);
         });
-        it('adds the new response with UNKNOWN status', () => {
-            expect(responses.get(6).status).toBe('UNKNOWN');
+        it('adds the new response with NEW status', () => {
+            responses.add();
+            expect(responses.get(6).status).toBe('NEW');
         });
-        it('forgets old responses if storage is exceeded', () => {
-            expect(responses.get(5).status).not.toBe('UNKNOWN');
-            responses.add(); // now we've got 5,6,7
-            responses.add(); // now we'd have 5,6,7,8, but…
-            expect(responses.get(5).status).toBe('UNKNOWN');
+    });
+    describe('setStatus', () => {
+        it('sets status', () => {
+            const myResponses = (new ActionResponses({2: {status: Status.NEW, message: ''}}));
+            expect(myResponses.get(2).status).not.toBe(Status.PENDING);
+            myResponses.setCurrentStatus(Status.PENDING);
+            expect(myResponses.get(2).status).toBe(Status.PENDING);
+        });
+        it('sets message', () => {
+            const myResponses = (new ActionResponses({2: {status: Status.NEW, message: ''}}));
+            expect(myResponses.get(2).message).not.toBe('yay');
+            myResponses.setCurrentMessage('yay');
+            expect(myResponses.get(2).message).toBe('yay');
         });
     });
 });
--- src/entities/ActionResponses.ts
+++ src/entities/ActionResponses.ts
@@ -1,30 +1,56 @@
+export enum Status {
+    NEW = 'NEW', PENDING = 'PENDING', FAILED = 'FAILED', DONE = 'DONE', UNKNOWN = 'UNKNOWN',
+}
 
 export interface IRepoActionResponse {
-    status: 'PENDING'|'FAILED'|'DONE'|'UNKNOWN';
+    status: Status;
     message: string;
 }
 
 const unknownRepoActionResponse: IRepoActionResponse = {
-    status: 'UNKNOWN',
+    status: Status.UNKNOWN,
     message: 'I have forgotten that action',
 };
 
 export class ActionResponses {
-
+    private maxId = -1;
     public constructor(
         private responsesDto: { [id: number]: IRepoActionResponse; } = {},
         private maxResponseStorageCount = 100,
-    ) {}
+    ) {
+        this.maxId = Math.max.apply(this, Object.getOwnPropertyNames(responsesDto).map(Number));
+        if (isNaN(this.maxId)) {
+            throw new Error('responsesDto seems to have contained a non-numeric index!');
+        }
+    }
 
     public get(id: number): IRepoActionResponse {
-        return unknownRepoActionResponse;
+        return this.responsesDto[id] || unknownRepoActionResponse;
     }
 
     public add(): number {
-        return -1;
+        this.maxId++;
+        this.responsesDto[this.maxId] = {
+            status: Status.NEW,
+            message: 'unknown',
+        };
+        delete this.responsesDto[this.maxId - this.maxResponseStorageCount];
+        return this.maxId;
     }
 
-    public setStatus(id: number, status: string): void {}
+    public setCurrentStatus(status: Status): void {
+        const response = this.responsesDto[this.maxId];
+        if (!response) {
+            throw new Error('wtf');
+        }
+        response.status = status;
+    }
 
-    public setMessage(id: number, message: string): void {}
+    public setCurrentMessage(message: string): void {
+        const response = this.responsesDto[this.maxId];
+        if (!response) {
+            throw new Error('wtf');
+        }
+        response.message = message;
+    }
 }
